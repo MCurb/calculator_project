83ca0d8d94cada68a5f4f15d3018cec70a89cb11
MCurb <marcoscurbeco03@gmail.com>
2025-05-23 06:48:16 -0600

    Make the calculator and the code more functional
    Now if the program won't append new input to the result. It will perform
    another operation if another operator is selected. It won't erase the
    display content after an operator has been selected, but after another
    number has been typed. Also the code is now more readable.

--- javascript.js
+++ javascript.js
@@ -11,81 +11,149 @@ function subtract(num1, num2) {
   return num1 - num2;
 }
 
-let firstNum = 0;
-let secondNum = 0;
-let operator = 0;
+let firstOperand = 0;
+let secondOperand = 0;
+let activeOperator = 0;
+let result = 0;
+let justCalculated = false;
 
-function operate(firstNum, secondNum, operator) {
-    firstNum = +firstNum;
-    secondNum = +secondNum;
-    if (operator === "divide") {
-        //First check if the second number is not 0
-        if (secondNum === 0) {
-            displayContent.textContent = "Fuck you bitch"
-        } else {
-            let result = divide(firstNum, secondNum);
-        //Convert the function result to string and test if it's larger than 10 characters
-        if (result.toString().length > 10) {
-            //if result > than 10, return a value with maximum 2 numbers after the point
-            displayContent.textContent = result.toFixed(2)
-        } else {displayContent.textContent = result;}}
-         
-    } else if (operator === "multiply") {
-        let result = multiply(firstNum, secondNum);
-        if (result.toString().length > 10) {
-            //if result > than 10, display the value in scientific notation
-            displayContent.textContent = result.toExponential(4)
-        } else {displayContent.textContent = result;}
-    } else if (operator === "add") {
-        let result = add(firstNum, secondNum);
-        if (result.toString().length > 10) {
-            displayContent.textContent = result.toExponential(4)
-        } else {displayContent.textContent = result;} 
-    } else if (operator === "subtract") {
-        displayContent.textContent = subtract(firstNum, secondNum);
-    }
+function operate(firstOperand, secondOperand, activeOperator) {
+  //Convert string values into numbers
+  firstOperand = +firstOperand;
+  secondOperand = +secondOperand;
+
+  //Check what's the active operator and call the related function
+  switch (activeOperator) {
+
+    case "divide":
+      if (secondOperand === 0) {
+        display.textContent = "lmao";
+      } else {
+        result = divide(firstOperand, secondOperand);
+        //If result has > 10 num reduce it to just 9 after 0, if not return result
+        display.textContent =
+          result.toString().length > 10 ? result.toFixed(6) : result;
+      }
+      break;
+
+    case "multiply":
+      result = multiply(firstOperand, secondOperand);
+      //If result has > 10 num display it in scientific notation, if not return result
+      display.textContent =
+        result.toString().length > 10 ? result.toExponential(4) : result;
+      break;
+
+    case "add":
+      result = add(firstOperand, secondOperand);
+
+      display.textContent =
+        result.toString().length > 10 ? result.toExponential(4) : result;
+      break;
+
+    case "subtract":
+      display.textContent = 
+      subtract(firstOperand, secondOperand);
+      break;
+
+    default:
+      return;
+  }
+  clearAll();
+}
+
+function clearAll() {
+  firstOperand = "";
+  secondOperand = "";
+  activeOperator = "";
 }
 
 const container = document.querySelector(".container");
-const displayContent = document.querySelector(".display");
-const divideBtn = document.querySelector(".divide-operator");
+const display = document.querySelector(".display");
 
-function handelClick(e) {
-  if (e.target.matches(".number")) {
-    //every time a number button is pressed it will update the display content text if the are not more than 10 numbers
-    if(displayContent.textContent.length >= 10) {
-        displayContent.removeEventListener("click", handelClick)
-    } else {displayContent.textContent = displayContent.textContent + e.target.textContent;}
+//Take the second operand from the screen, perform the calculation and make the result the first operand for the next calculation with the selected operator, also leave a flag that a calculation was performed.
+function handleSecondOperation() {
+  secondOperand = display.textContent;
+  operate(firstOperand, secondOperand, activeOperator);
+  firstOperand = display.textContent;
+  justCalculated = true;
+}
+
+container.addEventListener("click", handleClick);
+
+//Check what was the clicked button inside container
+function handleClick(e) {
+
+  if (e.target.matches(".number-btn")) {
     
+    if (justCalculated === true) {
+      display.textContent = "";
+      display.textContent = e.target.textContent;
+      justCalculated = false;
+    } else {
+      if (display.textContent.length >= 10) {
+        display.removeEventListener("click", handleClick);
+      } else {
+        display.textContent += e.target.textContent;
+      }
+    }
+
   } else if (e.target.matches(".divide-operator")) {
-    //update the firstNum and operator variables
-    firstNum = displayContent.textContent;
-    operator = "divide";
-    divideBtn.setAttribute("style", "background: blue");
-    //errase the text content and wait for the second number input
-    displayContent.textContent = "";
+
+    if (activeOperator != "") {
+      handleSecondOperation();
+      activeOperator = "divide";
+    } else {
+      firstOperand = display.textContent;
+      activeOperator = "divide";
+      justCalculated = true;
+    }
+
   } else if (e.target.matches(".multiply-operator")) {
-    firstNum = displayContent.textContent;
-    operator = "multiply";
-    displayContent.textContent = "";
+
+    if (activeOperator != "") {
+      handleSecondOperation();
+      activeOperator = "multiply";
+    } else {
+      firstOperand = display.textContent;
+      activeOperator = "multiply";
+      justCalculated = true;
+    }
+
   } else if (e.target.matches(".add-operator")) {
-    firstNum = displayContent.textContent;
-    operator = "add";
-    displayContent.textContent = "";
+
+    if (activeOperator != "") {
+      handleSecondOperation();
+      activeOperator = "add";
+    } else {
+      firstOperand = display.textContent;
+      activeOperator = "add";
+      justCalculated = true;
+    }
+
   } else if (e.target.matches(".subtract-operator")) {
-    firstNum = displayContent.textContent;
-    operator = "subtract";
-    displayContent.textContent = "";
+
+    if (activeOperator != "") {
+      handleSecondOperation();
+      activeOperator = "subtract";
+    } else {
+      firstOperand = display.textContent;
+      activeOperator = "subtract";
+      justCalculated = true;
+    }
+
   } else if (e.target.matches(".equal-operator")) {
-    //update the second num variable
-    secondNum = displayContent.textContent;
-    //call the operate function to make the calculation
-    operate(firstNum, secondNum, operator);
+
+    secondOperand = display.textContent;
+    operate(firstOperand, secondOperand, activeOperator);
+    justCalculated = true;
+
+
   } else if (e.target.matches(".all-clear")) {
-    displayContent.textContent = "";
-    firstNum = 0;
-    secondNum = 0;
-    operator = 0;
+
+    display.textContent = 0;
+    firstOperand = "";
+    secondOperand = "";
+    activeOperator = "";
+    result = "";
   }
 }
-container.addEventListener("click", handelClick);
